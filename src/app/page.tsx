ecdfa478fdafff456086ba3c96f3c6eaa6b771cd
cloyd <cloyd.dean@gmail.com>
2025-02-11 20:53:10 +0800

    chore: comment out Skills section in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section id="skills">
+      {/* <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 9}>
             <h2 className="text-xl font-bold">Skills</h2>
@@ -127,7 +127,7 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section id="services">
         <div className="space-y-12 w-full py-12">
