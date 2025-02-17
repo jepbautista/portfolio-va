e040c142ca3953b6cdde7f9bfcdb652c8fd8b9f6
cloyd <cloyd.dean@gmail.com>
2025-02-17 22:06:00 +0800

    perf: update image formats to webp for improved performance

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
   description: "A freelance E-Commerce Specialist | Virtual Assistant",
   summary:
     "I offer a range of professional services to help businesses grow and streamline their operations. As an Amazon FBA Specialist, I optimize product listings, manage inventory, troubleshoot account issues, and run PPC campaigns to enhance visibility and boost sales. My web development services focus on creating responsive, SEO-friendly, and visually appealing websites tailored to your business needs for an engaging user experience. I also provide professional photo and video editing, including color correction, retouching, background removal, transitions, and dynamic effects to create polished, captivating content. Additionally, I offer administrative support, managing daily tasks such as email and calendar management, data entry, document preparation, and file organization to improve efficiency and productivity. I am dedicated to delivering high-quality, reliable services tailored to meet your business goals.",
-  avatarUrl: "/portfolio-va/me.jpg",
+  avatarUrl: "/portfolio-va/me.webp",
   skills: [
     "Amazon FBA",
     "Amazon PPC",
@@ -72,7 +72,7 @@ export const DATA = {
       school: "Pangasinan State University",
       href: "https://main.psu.edu.ph/",
       degree: "Bachelor of Science in Computer Science",
-      logoUrl: "/portfolio-va/psu_logo.png",
+      logoUrl: "/portfolio-va/psu_logo.webp",
       start: "2009",
       end: "2013",
     },
@@ -80,7 +80,7 @@ export const DATA = {
       school: "Udemy",
       href: "https://www.udemy.com/",
       degree: "Photoshop Master Course",
-      logoUrl: "/portfolio-va/udemy_logo.png",
+      logoUrl: "/portfolio-va/udemy_logo.webp",
       start: "-",
       end: "-",
     },
@@ -88,7 +88,7 @@ export const DATA = {
       school: "QuickPro Online Professional Academy",
       href: "https://theonepercentfreelancer.com/",
       degree: "Amazon FBA, Bookkeeping, Web Development, Copywiting",
-      logoUrl: "/portfolio-va/quickpro_logo.png",
+      logoUrl: "/portfolio-va/quickpro_logo.webp",
       start: "-",
       end: "-",
     },
@@ -110,7 +110,7 @@ export const DATA = {
         "Keyword Research",
         "Product Sourcing",
       ],
-      image: "/portfolio-va/amazon-pic.jpg",
+      image: "/portfolio-va/amazon-pic.webp",
     },
 
     {
@@ -127,7 +127,7 @@ export const DATA = {
         "Canva",
         "Quickbooks",
       ],
-      image: "/portfolio-va/admin-support-img.jpg",
+      image: "/portfolio-va/admin-support-img.webp",
     },
 
     {
@@ -136,11 +136,8 @@ export const DATA = {
       active: true,
       description:
         "Transform raw photos and footage into polished, visually captivating content. Services include color correction, retouching, background removal, and photo enhancements. For video editing, I offer cutting, transitions, audio synchronization, and dynamic effects to create engaging, high-quality videos. With attention to detail and creative storytelling, I help bring your vision to life.",
-      technologies: [
-        "Photoshop",
-        "Premiere Pro",
-      ],
-      image: "/portfolio-va/ps-pro-img.jpg",
+      technologies: ["Photoshop", "Premiere Pro"],
+      image: "/portfolio-va/ps-pro-img.webp",
     },
 
     {
@@ -149,43 +146,34 @@ export const DATA = {
       active: true,
       description:
         "Design and develop responsive, user-friendly websites tailored to your business needs. Services include front-end development, custom coding, website optimization, and mobile compatibility. I ensure fast-loading, SEO-friendly, and visually appealing websites for an engaging user experience. Whether building a portfolio, business site, or landing page, I deliver functional and aesthetically pleasing solutions.",
-      technologies: [
-        "HTML",
-        "CSS",
-        "Java Script",
-      ],
-      image: "/portfolio-va/wev-dev-img.jpg",
+      technologies: ["HTML", "CSS", "Java Script"],
+      image: "/portfolio-va/wev-dev-img.webp",
     },
-
-
-
-
-
   ],
   testimonials: [
     {
       name: "Maria Crisel Robles",
       username: "@crisel",
       body: "Jeffrey is one of the most skilled RevOps professionals at SellerX. He’s always ready to help and answer any questions. Collaborating with him is effortless, as he manages tasks independently while ensuring the brand’s needs are met and the team is well-supported.",
-      img: "/portfolio-va/crisel-img.jpg",
+      img: "/portfolio-va/crisel-img.webp",
     },
     {
       name: "Charles Phillips",
       username: "@charles",
       body: "He has an excellent eye for detail, and a strong understanding of Amazon marketplace, pricing dynamics, buy box management. He also has shown strong commercial understanding of P/L management often producing detailed and important reports on product profitability and providing suggested actions. He has a great knowledge of Amazon and Ecom and is a great asset to any team.",
-      img: "/portfolio-va/charles-img.jpg",
+      img: "/portfolio-va/charles-img.webp",
     },
     {
       name: "Selcuk Cullu",
       username: "@selcuk",
       body: "I am pleased to write a testimonial for Jeffrey, who has been an invaluable member of our team. We have been working together for the past three months, and during this time, I have been continually impressed by his positive attitude, dedication, and eagerness to learn. Jeffrey's expertise in Amazon listing and campaign management has been a significant asset to our organization. His ability to adapt and manage processes with ease has streamlined our operations and greatly improved our online presence. He approaches every task with enthusiasm and is always willing to go the extra mile to ensure success. In summary, Jeffrey is a highly skilled professional whose dedication, expertise, and supportive nature make him an exceptional colleague. I am confident that he will continue to excel in.",
-      img: "/portfolio-va/selcuk-img.jpg",
+      img: "/portfolio-va/selcuk-img.webp",
     },
     {
       name: "Mariana Lemos",
       username: "@mariana",
       body: "I've had the pleasure of collaborating with Jeffrey on numerous projects, and I can confidently say that he is a standout performer in every aspect. His dedication to delivering outstanding results sets him apart from the crowd. Jeffrey's expertise in the Amazon Seller Center is truly remarkable. From meticulous listing review and optimization to strategic PPC management, he consistently drives significant improvements in our sales performance. His proficiency in handling image and attribute uploads has streamlined our processes and enhanced our product visibility on the platform. Beyond his technical skills, Jeffrey's interpersonal abilities and teamwork shine brightly. He effortlessly collaborates with colleagues, demonstrating excellent communication skills.",
-      img: "/portfolio-va/mariana-img.jpg",
+      img: "/portfolio-va/mariana-img.webp",
     },
   ],
 } as const;
